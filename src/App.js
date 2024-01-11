96509900fd905b07bd7d2a80e5a2c5621b6a3f1a
orianamachado66 <orianamachado66@gmail.com>
2024-01-10 20:07:44 -0800

    Created a Dictionary component with a search engine

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Dictionary App</h1>
+    <div className="container">
+      <div className="App">
+        <h1>Dictionary App</h1>
+
+        <main>
+          <Dictionary />
+        </main>
+
+        <footer className="text-center">
+          This project was coded by{" "}
+          <a
+            href="https://www.linkedin.com/in/oriana-machado-771014130/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            Oriana Machado{" "}
+          </a>{" "}
+          and it is{" "}
+          <a
+            href="https://github.com/orianamachado66/dictionary-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            open-sourced{" "}
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
