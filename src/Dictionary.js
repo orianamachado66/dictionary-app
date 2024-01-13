1098713b010a09832c31cbef568f9db10bbe67f9
orianamachado66 <orianamachado66@gmail.com>
2024-01-13 14:34:47 -0800

    Added a new component to display the word meanings

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,17 @@
 import React, { useState } from "react";
 import axios from "axios";
+import Results from "./Results";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState(null);
+
+  function handleResponse(response) {
+    setResults(response.data[0]);
+  }
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword}`);
 
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
@@ -24,6 +29,8 @@ export default function Dictionary() {
           onChange={handleKeywordChange}
         />
         <input type="submit" />
+
+        <Results results={results} />
       </form>
     </div>
   );
